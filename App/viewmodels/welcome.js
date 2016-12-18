96ff992b04d007f434f1518ef35141fcd38e8fc2
Painatalio <pedro360elite@hotmail.com>
2016-12-18 18:10:29 +0000

    Add files via upload

--- App/viewmodels/welcome.js
+++ App/viewmodels/welcome.js
@@ -12,6 +12,26 @@
             'App Bundling and Optimization',
             'Use any Backend Technology',
             'Built on top of jQuery, Knockout & RequireJS'
-        ]
+        ],
+        closeModal: function () {
+            var s = document.getElementsByTagName("IFRAME")[0];
+            s.src = '';
+        },
+        trailer: function () {
+            var s = document.getElementsByTagName("IFRAME")[0];
+            s.src = "//www.dailymotion.com/embed/video/x42pgdt?autoplay=1&quality='720'";
+        },
+        trailer2: function () {
+            var s = document.getElementsByTagName("IFRAME")[0];
+            s.src = "//www.dailymotion.com/embed/video/x4m9s8d?autoplay=1&quality='720'";
+        },
+        trailer3: function () {
+            var s = document.getElementsByTagName("IFRAME")[0];
+            s.src = "//www.dailymotion.com/embed/video/x55flyx?autoplay=1&quality='720'"
+        },
+        trailer4: function () {
+            var s = document.getElementsByTagName("IFRAME")[0];
+            s.src = "//www.dailymotion.com/embed/video/x441b0i?autoplay=1&quality='720'";
+        }
     };
 });
