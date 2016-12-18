1c7319c724835638700c2b57e162e05f18e78c6e
Painatalio <pedro360elite@hotmail.com>
2016-12-18 21:17:41 +0000

    Add files via upload

--- App/viewmodels/welcome.js
+++ App/viewmodels/welcome.js
@@ -1,18 +1,5 @@
 ﻿define(['plugins/http', 'durandal/app', 'knockout'], function () {
     return {
-        displayName: 'Bemvindo à ITW App!',
-        description: 'Esta plataforma servirá para o desenvolvimento de aplcações móveis na unidade curricular de Introdução às tecnologias Web (ITW) no ano letivo de 2015/16.',
-        features: [
-            'Clean MV* Architecture',
-            'JS & HTML Modularity',
-            'Simple App Lifecycle',
-            'Eventing, Modals, Message Boxes, etc.',
-            'Navigation & Screen State Management',
-            'Consistent Async Programming w/ Promises',
-            'App Bundling and Optimization',
-            'Use any Backend Technology',
-            'Built on top of jQuery, Knockout & RequireJS'
-        ],
         closeModal: function () {
             var s = document.getElementsByTagName("IFRAME")[0];
             s.src = '';
