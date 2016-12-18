2d8c49319e3ab6d6d90fa34292a2766fd0f7522a
Painatalio <pedro360elite@hotmail.com>
2016-12-18 21:26:32 +0000

    Update welcome.js

--- App/viewmodels/welcome.js
+++ App/viewmodels/welcome.js
@@ -6,19 +6,19 @@
         },
         trailer: function () {
             var s = document.getElementsByTagName("IFRAME")[0];
-            s.src = "//www.dailymotion.com/embed/video/x42pgdt?autoplay=1&quality='720'";
+            s.src = "//www.dailymotion.com/embed/video/x42pgdt";
         },
         trailer2: function () {
             var s = document.getElementsByTagName("IFRAME")[0];
-            s.src = "//www.dailymotion.com/embed/video/x4m9s8d?autoplay=1&quality='720'";
+            s.src = "//www.dailymotion.com/embed/video/x4m9s8d";
         },
         trailer3: function () {
             var s = document.getElementsByTagName("IFRAME")[0];
-            s.src = "//www.dailymotion.com/embed/video/x55flyx?autoplay=1&quality='720'"
+            s.src = "//www.dailymotion.com/embed/video/x55flyx"
         },
         trailer4: function () {
             var s = document.getElementsByTagName("IFRAME")[0];
-            s.src = "//www.dailymotion.com/embed/video/x441b0i?autoplay=1&quality='720'";
+            s.src = "//www.dailymotion.com/embed/video/x441b0i";
         }
     };
 });
