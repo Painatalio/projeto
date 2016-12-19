8f151408a4c8e30b1ba6a3412858a27daa7508d1
Painatalio <pedro360elite@hotmail.com>
2016-12-19 19:48:43 +0000

    Update Ajuda.js

--- App/viewmodels/Ajuda.js
+++ App/viewmodels/Ajuda.js
@@ -1,17 +1,4 @@
 ﻿define(['plugins/http', 'durandal/app', 'knockout'], function () {
     return {
-        validar: $('form').submit(function (event) {
-            var retVal = true;
-            if ($.trim($('#nome').val()).length < 3) {
-                if ($('#nome_Error').hasClass("no_error"))
-                    $('#nome_Error').removeClass("no_error").addClass("error");
-                retVal = false;
-            }
-            else {
-                if ($('#nome_Error').hasClass("error"))
-                    $('#nome_Error').removeClass("error").addClass("no_error");
-            };
-            return retVal;
-        }),
     };
 });
